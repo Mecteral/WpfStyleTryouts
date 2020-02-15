cc8f9d174c0cb854766bbe3887f60f75128552f8
Dominik Siegele <Siegele.Dominik@gmx.at>
2020-02-15 12:32:33 +0100

    Added Solution and Tab style demo;

---
+++ WpfStyleTryouts/FrameworkTryouts/Tabs/TabControlDemo.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace FrameworkTryouts.Tabs
+{
+    /// <summary>
+    /// Interaction logic for TabControlDemo.xaml
+    /// </summary>
+    public partial class TabControlDemo : UserControl
+    {
+        public TabControlDemo()
+        {
+            InitializeComponent();
+        }
+    }
+}
